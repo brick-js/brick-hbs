c5cdc68833de94c090325996576dfbbee67c7595
harttle <yangjvn@126.com>
2016-03-29 21:42:59 +0800

    remove demo

--- demo/modules/default/server.js
+++
@@ -1,7 +0,0 @@
-var User = require('../../models/user.js');
-var Cat = require('../../models/cat.js');
-var debug = require('debug')('demo:default');
-
-exports.resolver = function(req, done, fail){
-    done({ title: 'Brick.js Demo' });
-};
--- demo/modules/homepage/server.js
+++
@@ -1,8 +0,0 @@
-var User = require('../../models/user.js');
-var Cat = require('../../models/cat.js');
-var debug = require('debug')('demo:homepage');
-
-exports.url = ['/', '/homepage'];
-exports.resolver = function(req, done, fail){
-    done();
-};
